3c254e7eb4801da8c7b504901d3db468fe88c088
Kenneth Chittaphong <kennethchittaphong@gmail.com>
2022-06-15 18:45:50 -0500

    package.json

--- src/scripts/helpers/auth.js
+++ src/scripts/helpers/auth.js
@@ -3,11 +3,13 @@ import 'firebase/auth';
 import loginButton from '../components/loginButton';
 import logoutButton from '../components/logoutButton';
 import firebaseConfig from '../../api/apiKeys';
+import startApp from './utilities/startApp';
 
 const checkLoginStatus = () => {
   firebase.initializeApp(firebaseConfig);
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
+      startApp(user);
       // person is logged in do something...
       logoutButton();
     } else {
