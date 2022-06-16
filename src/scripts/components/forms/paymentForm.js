81c1b494d61ddd540c7d3dc720228feafa2a4e0f
Emily Stroud <emilystroud93@gmail.com>
2022-06-15 20:56:13 -0500

    saved changes

--- src/scripts/components/forms/paymentForm.js
+++ src/scripts/components/forms/paymentForm.js
@@ -4,12 +4,12 @@ import renderToDom from '../../helpers/utilities/renderToDom';
 const paymentForm = () => {
   clearDom();
   const domString = `
-  
+  <form id="payment">
   <select class="form-select" aria-label="Default select example">
       <option selected>Payment Type</option>
       <option value="1">Cash</option>
-      <option value="2">Card</option>
-      <option value="3">Three</option>
+      <option value="2">Credit</option>
+      <option value="3">Mobile</option>
     <div class="input-group mb-3">
       <span class="input-group-text">$</span>
       <input type="text" class="form-control" aria-label="Tip Amount">
@@ -17,8 +17,8 @@ const paymentForm = () => {
     </div>
   </select>
   <button type="submit" class="btn btn-primary">Close Order
-      </button>
-  
+    </button>
+  </form>
   `;
 
   renderToDom('#form-container', domString);
