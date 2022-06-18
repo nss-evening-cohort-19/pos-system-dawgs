807414305f66d3ecd3c4a0c760669002976c62e0
Imad Ottallah <imadotta@gmail.com>
2022-06-18 10:53:09 -0500

    added html to food forms and api calls

--- src/scripts/helpers/utilities/startApp.js
+++ src/scripts/helpers/utilities/startApp.js
@@ -1,13 +1,15 @@
 import navEvents from '../../components/events/navEvents';
+import addFood from '../../components/forms/foodForm';
 import logoutButton from '../../components/logoutButton';
-import homeLoggedIn from '../../components/pages/homeLoggedIn';
+// import homeLoggedIn from '../../components/pages/homeLoggedIn';
 import navBar from '../../components/pages/navBar';
 import domBuilder from '../domBuilder';
 
 const startApp = (user) => {
   domBuilder();
   navBar();
-  homeLoggedIn();
+  // homeLoggedIn();
+  addFood();
   logoutButton();
   navEvents(user.uid);
 };
