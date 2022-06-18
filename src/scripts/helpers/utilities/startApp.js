23f1cd793fae10071d1ed94e595686d8cdc479ed
Imad Ottallah <imadotta@gmail.com>
2022-06-18 11:35:17 -0500

    corrected errors in food form

--- src/scripts/helpers/utilities/startApp.js
+++ src/scripts/helpers/utilities/startApp.js
@@ -2,9 +2,9 @@
 import navEvents from '../../components/events/navEvents';
 import addFood from '../../components/forms/foodForm';
 import logoutButton from '../../components/logoutButton';
-import paymentForm from '../../components/forms/paymentForm';
+// import paymentForm from '../../components/forms/paymentForm';
 // import orderFormOnDom from '../../components/forms/orderForm';
-import viewCart from '../../components/pages/cart';
+// import viewCart from '../../components/pages/cart';
 // import orderCardsOnDom from '../../components/pages/allOrders';
 // import homeLoggedIn from '../../components/pages/homeLoggedIn';
 import navBar from '../../components/pages/navBar';
@@ -16,8 +16,8 @@ const startApp = (user) => {
   // homeLoggedIn();
   addFood();
   // orderFormOnDom();
-  paymentForm();
-  viewCart();
+  // paymentForm();
+  // viewCart();
   logoutButton();
   navEvents(user.uid);
   // getOrders().then((orderArray) => orderCardsOnDom(orderArray));
