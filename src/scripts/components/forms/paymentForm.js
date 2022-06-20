23a9a0eb4e00963976da760329724e13424bbbe9
Emily Stroud <emilystroud93@gmail.com>
2022-06-20 17:24:37 -0500

    added promises and API calls to paymentData

--- src/scripts/components/forms/paymentForm.js
+++ src/scripts/components/forms/paymentForm.js
@@ -1,22 +1,23 @@
 // import clearDom from '../../helpers/utilities/clearDom';
 import renderToDom from '../../helpers/utilities/renderToDom';
 
-const paymentForm = () => {
+const paymentForm = (obj = {}) => {
   // clearDom();
   const domString = `
-  <form id="payment">
+  <form id="${obj.firebaseKey ? `update-payment--${obj.firebaseKey}` : 'submit-payment'}">
   <select class="form-select" aria-label="Default select example">
-      <option selected>Payment Type</option>
-      <option value="1">Cash</option>
-      <option value="2">Credit</option>
-      <option value="3">Mobile</option>
+      <option selected>${obj.type || ''}</option>
+      <option value="${obj.cash || ''}">Cash</option>
+      <option value="${obj.credit || ''}">Credit/Debit</option>
+      <option value="${obj.mobile || ''}">Mobile</option>
+      <option value="${obj.check || ''}">Check</option>
     <div class="input-group mb-3">
       <span class="input-group-text">$</span>
-      <input type="text" class="form-control" aria-label="Tip Amount">
+      <input type="text" class="form-control" aria-label="${obj.tip || ''}">
       <span class="input-group-text">.00</span>
     </div>
   </select>
-  <button type="submit" class="btn btn-primary">Close Order
+  <button type="submit" class="btn btn-success">Close Order
     </button>
   </form>
   `;
