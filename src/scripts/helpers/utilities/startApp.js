ef826bad6a958b08821a6d0dbb9db63787b1944b
Kenneth Chittaphong <kennethchittaphong@gmail.com>
2022-06-20 21:15:07 -0500

    merged

--- src/scripts/helpers/utilities/startApp.js
+++ src/scripts/helpers/utilities/startApp.js
@@ -1,13 +1,10 @@
-import { getOrders } from '../../../api/orderData';
 // import { getFood } from '../../../api/foodData';
 import navEvents from '../../components/events/navEvents';
 // import addFood from '../../components/forms/foodForm';
 import logoutButton from '../../components/logoutButton';
 // import { showFoods } from '../../components/pages/food';
 // import paymentForm from '../../components/forms/paymentForm';
-import orderFormOnDom from '../../components/forms/orderForm';
 // import viewCart from '../../components/pages/cart';
-import { orderCardsOnDom } from '../../components/pages/allOrders';
 import homeLoggedIn from '../../components/pages/homeLoggedIn';
 import navBar from '../../components/pages/navBar';
 import domBuilder from '../domBuilder';
@@ -19,7 +16,6 @@ const startApp = (user) => {
   navBar();
   homeLoggedIn();
   // addFood();
-  orderFormOnDom();
   // paymentForm();
   // viewCart();
   logoutButton();
@@ -28,9 +24,7 @@ const startApp = (user) => {
   orderFormEvents(user.uid);
   // getFood().then((foodArray) => showFoods(foodArray));
   homeLoggedIn();
-  getOrders().then((orderArray) => orderCardsOnDom(orderArray));
   domEvents();
-  getOrders().then((orderArray) => orderCardsOnDom(orderArray));
 };
 
 export default startApp;
