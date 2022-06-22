8ab7f4e62b0fb4026065878aa9de404b1204fa6b
Emily Stroud <emilystroud93@gmail.com>
2022-06-22 18:30:45 -0500

    removed eslint disabler line in auth.js

--- src/scripts/helpers/auth.js
+++ src/scripts/helpers/auth.js
@@ -1,7 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import loginButton from '../components/loginButton';
-// eslint-disable-next-line no-alert
 import startApp from './utilities/startApp';
 import logoutButton from '../components/logoutButton';
 import firebaseConfig from '../../api/apiKeys';
