8de8b089213a1d25c86be2e1608ca7d9200dc527
Imad Ottallah <imadotta@gmail.com>
2022-06-21 20:58:36 -0500

    updated

--- src/scripts/helpers/utilities/startApp.js
+++ src/scripts/helpers/utilities/startApp.js
@@ -1,36 +1,34 @@
 // import { getFood } from '../../../api/foodData';
 import navEvents from '../../components/events/navEvents';
-import addFood from '../../components/forms/foodForm';
+// import addFood from '../../components/forms/foodForm';
 import logoutButton from '../../components/logoutButton';
 // import { showFoods } from '../../components/pages/food';
 // import paymentForm from '../../components/forms/paymentForm';
 // import orderFormOnDom from '../../components/forms/orderForm';
 // import { viewCart, emptyCart } from '../../components/pages/cart';
 // import orderCardsOnDom from '../../components/pages/allOrders';
-// import homeLoggedIn from '../../components/pages/homeLoggedIn';
 import navBar from '../../components/pages/navBar';
 import domBuilder from '../domBuilder';
 import domEvents from '../../components/events/domEvents';
 import { orderDomEvents, orderFormEvents } from '../../components/events/orderEvents';
-import homeLoggedIn from '../../components/pages/homeLoggedIn';
+// import homeLoggedIn from '../../components/pages/homeLoggedIn';
 
 const startApp = (user) => {
   domBuilder();
   navBar();
-  addFood();
+  // addFood();
   // orderFormOnDom();
   // paymentForm();
   // viewCart(user);
   // emptyCart(user);
-  homeLoggedIn();
+  // homeLoggedIn();
   logoutButton();
   navEvents(user.uid);
   orderDomEvents(user.uid);
   orderFormEvents(user.uid);
   domEvents();
-  // getFood().then((foodArray) => showFoods(foodArray));
   // getOrders().then((orderArray) => orderCardsOnDom(orderArray));
-//   getFood().then((foodArray) => showFoods(foodArray));
+  // getFood().then((foodArray) => showFoods(foodArray));
 };
 
 export default startApp;
