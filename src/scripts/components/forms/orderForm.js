a30c8e6993e1bbaf56f9ab36d7f888fece6cd121
Shalane Proctor <shalane088@gmail.com>
2022-06-22 13:02:05 -0500

    added terinaries to make drop down on edit order render when editing. Made drop down required and place holder unselectable when creating or editing order

--- src/scripts/components/forms/orderForm.js
+++ src/scripts/components/forms/orderForm.js
@@ -21,11 +21,16 @@ const orderFormOnDom = (order = {}) => {
       <input type="email" class="form-control" id="customerEmail" placeholder="name@example.com" value="${
   order.email || ''
 }" required>
+<label for="orderType" class="form-label">Order Type</label>
       <select id="orderType" class="form-select" aria-label="Default select example" required>
-        <label for="orderType" class="form-label">Order Type</label>
-        <option id="orderType" selected>Select An Order Type</option>
-        <option value="value="${order.type || 'Phone'}">Phone</option>
-        <option value="${order.type || 'In-Person'}">In-Person</option>
+        <option value="">Select An Order Type</option>
+        <option value="Phone" ${
+  order.type === 'Phone' ? 'selected' : ''
+}>Phone</option>
+        <option value="In-Person" ${
+  order.type === 'In-Person' ? 'selected' : ''
+}
+}>In-Person</option>
       </select>
       <button type="submit" class="btn btn-primary mt-3">Create/Edit Order</button>
     </div>
