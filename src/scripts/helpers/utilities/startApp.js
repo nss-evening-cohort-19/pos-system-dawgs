6edbd4ef142d12ab8ec4a673a97cd80db57c8118
Emily Stroud <emilystroud93@gmail.com>
2022-06-22 18:39:51 -0500

    re-workimg forEach error

--- src/scripts/helpers/utilities/startApp.js
+++ src/scripts/helpers/utilities/startApp.js
@@ -1,25 +1,25 @@
-import { getFood } from '../../../api/foodData';
+// import { getFood } from '../../../api/foodData';
 import navEvents from '../../components/events/navEvents';
-// import addFood from '../../components/forms/foodForm';
+import addFood from '../../components/forms/foodForm';
 import logoutButton from '../../components/logoutButton';
-import { showFoods } from '../../components/pages/food';
+// import { showFoods } from '../../components/pages/food';
 import paymentForm from '../../components/forms/paymentForm';
-// import orderFormOnDom from '../../components/forms/orderForm';
+import orderFormOnDom from '../../components/forms/orderForm';
 // import { viewCart, emptyCart } from '../../components/pages/cart';
-import { orderCardsOnDom } from '../../components/pages/allOrders';
+// import { orderCardsOnDom } from '../../components/pages/allOrders';
 import homeLoggedIn from '../../components/pages/homeLoggedIn';
 import navBar from '../../components/pages/navBar';
 import domBuilder from '../domBuilder';
 import domEvents from '../../components/events/domEvents';
 import { orderDomEvents, orderFormEvents } from '../../components/events/orderEvents';
-import { viewCart } from '../../components/pages/cart';
-import { getOrders } from '../../../api/orderData';
+// import { getOrders } from '../../../api/orderData';
+// import { viewCart } from '../../components/pages/cart';
 
 const startApp = (user) => {
   domBuilder();
   navBar();
-  // addFood();
-  // orderFormOnDom();
+  addFood();
+  orderFormOnDom();
   paymentForm();
   // viewCart(user);
   // emptyCart(user);
@@ -29,9 +29,9 @@ const startApp = (user) => {
   orderDomEvents(user.uid);
   orderFormEvents(user.uid);
   domEvents();
-  getFood().then((foodArray) => showFoods(foodArray));
-  getOrders().then((orderArray) => orderCardsOnDom(orderArray));
-  getFood().then((foodArray) => viewCart(foodArray));
+  // getFood().then((foodArray) => showFoods(foodArray));
+  // getOrders().then((orderArray) => orderCardsOnDom(orderArray));
+  // getFood().then((foodArray) => viewCart(foodArray));
 };
 
 export default startApp;
